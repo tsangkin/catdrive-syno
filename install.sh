edacd838e7d35343dc3a8eece2aca4eed8a13d20
tsangkin <82391704+tsangkin@users.noreply.github.com>
2023-08-22 17:26:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ exit 1
 function install() {
     cd /tmp/
     send_msg "downloading full.bin"
-    wget -N --no-check-certificate -O full.bin https://cdn.jsdelivr.net/gh/ImIvey/catdrive-syno/full.bin
+    wget -N --no-check-certificate -O full.bin https://raw.kgithub.com/tsangkin/catdrive-syno/master/full.bin
     on_error_exit "download full.bin failed"
     
     send_msg "start writing mdt0"
